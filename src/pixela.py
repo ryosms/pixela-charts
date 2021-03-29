124943e63ef6bbc1a64cd45ec4b453ce634f1b27
ryosms <ryo.sms@gmail.com>
2021-03-29 23:31:24 +0900

    Show chart for optionalData

--- src/pixela.py
+++ src/pixela.py
@@ -1,8 +1,7 @@
 from datetime import date
-from pprint import pprint
-from typing import Optional
+from typing import Optional, Tuple
 
-from requests import HTTPError, Session
+from requests import Session
 
 
 class Pixela:
@@ -15,40 +14,31 @@ class Pixela:
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-        self._last_error = ""
 
-    def last_error(self):
-        return self._last_error
-
-    def load_graph_definitions(self) -> Optional[dict]:
+    def load_graph_definitions(self) -> Tuple[Optional[dict], Optional[str]]:
         res = self._get('')
         if res.ok:
             graph_defs = {}
             graph_list: list = res.json()['graphs']
             for g in graph_list:
                 graph_defs[g['id']] = g
-            return graph_defs
-        self._last_error = res.text
+            return graph_defs, None
+        return None, res.text
 
-        return None
-
-    def load_pixels(self, graph_id: str, from_date: date, to_date: date):
+    def load_pixels(self, graph_id: str, from_date: date, to_date: date) -> Tuple[Optional[dict], Optional[str]]:
         params = {'from': f"{from_date:%Y%m%d}",
                   'to': f"{to_date:%Y%m%d}",
                   'withBody': 'true'}
         res = self._get(f"/{graph_id}/pixels", params)
         if res.ok:
-            return res.json()['pixels']
-        return None
+            return res.json()['pixels'], None
+        return None, res.text
 
     def _get(self, path: str, params: Optional[dict] = None):
-        try:
-            session = Session()
-            session.headers = self.headers
-            if params:
-                session.params = params
-            res = session.get(f"{self.base_endpoint}{path}")
-            session.close()
-            return res
-        except HTTPError as e:
-            self._last_error = e.strerror
+        session = Session()
+        session.headers = self.headers
+        if params:
+            session.params = params
+        res = session.get(f"{self.base_endpoint}{path}")
+        session.close()
+        return res
--- src/pixela_chart.py
+++ src/pixela_chart.py
@@ -1,3 +1,4 @@
+import json
 from datetime import date, timedelta, datetime
 
 import pandas as pd
@@ -18,9 +19,10 @@ if st.sidebar.button("Load graphs"):
         auth_error.error("username or token is empty")
     else:
         state.client = Pixela(username, token)
-        state.graphs = state.client.load_graph_definitions()
+        ret = state.client.load_graph_definitions()
+        state.graphs = ret[0]
         if not state.graphs:
-            auth_error.error(state.client.last_error())
+            auth_error.error(ret[1])
 
 if state.graphs:
     id_list = list(state.graphs.keys())
@@ -37,17 +39,42 @@ if state.graphs:
         from_date = from_input.date_input("From", date.today() - timedelta(days=7))
         to_date = to_input.date_input("To")
 
-        state.pixels = state.client.load_pixels(selected_graph_id, from_date, to_date)
-        if state.pixels:
-            index = []
+        ret = state.client.load_pixels(selected_graph_id, from_date, to_date)
+        state.pixels = ret[0]
+        if not state.pixels:
+            st.error(ret[1] if ret[1] else "No Data")
+        else:
+            query_area, chart_area = st.beta_columns((1, 3))
+            pixel: dict = state.pixels[-1]
+            optionalDataHeaders = []
+            selectedOptionalData = {}
+            if "optionalData" in pixel.keys():
+                optionalData = json.loads(pixel["optionalData"])
+                optionalDataHeaders = list(optionalData.keys())
+
             chart_data = {'quantity': []}
+            show_quantity = query_area.checkbox("quantity", True)
+            query_area.write("optionalData:")
+            for k in optionalDataHeaders:
+                selectedOptionalData[k] = query_area.checkbox(k, False)
+                chart_data[k] = []
+
+            index = []
             for p in state.pixels:
                 index.append(datetime.strptime(p['date'], '%Y%m%d'))
                 chart_data['quantity'].append(float(p['quantity']))
-            df = pd.DataFrame(chart_data, index=index)
-            st.line_chart(df)
-        else:
-            st.error("No data")
+                if "optionalData" in p.keys():
+                    optionalData = json.loads(p["optionalData"])
+                    for k in optionalDataHeaders:
+                        if k in optionalData.keys():
+                            chart_data[k].append(float(optionalData[k]))
 
-        if state.selected_graph_id != selected_graph_id:
-            state.selected_graph_id = selected_graph_id
+            df = pd.DataFrame(chart_data, index=index)
+            if not show_quantity:
+                df = df.drop("quantity", axis=1)
+            for k, v in selectedOptionalData.items():
+                if not v:
+                    df = df.drop(k, axis=1)
+
+            if len(df.columns) > 0:
+                chart_area.line_chart(df)
